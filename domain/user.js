5c55d9b9f3d89bf67c174a4463a3a63925d164a7
Mantas Navickis <navickis@gmail.com>
2023-11-18 23:11:10 +0100

    updated

--- domain/user.js
+++ domain/user.js
@@ -3,11 +3,28 @@ const saltRounds = 10
 const bcrypt = require('bcrypt')
 const getUser = async (req, res) => {
   try {
-    const user = await User.find({ email: req.user.email })
+    const user = await User.findOne({ email: req.user.email })
     if (!user) {
       return res.status(404).json({ message: 'User not found' })
     }
-    res.status(200).json(user)
+    const userDataToSend = {
+      email: user.email,
+      password: '',
+      profile: [
+        {
+          firstName: user.profile[0].firstName,
+          lastName: user.profile[0].lastName,
+          additionalInfo: [
+            {
+              birthday: user.profile[0].additionalInfo[0].birthday,
+              phoneNumber: user.profile[0].additionalInfo[0].phoneNumber
+            }
+          ]
+        }
+      ]
+    }
+
+    res.status(200).json(userDataToSend)
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
